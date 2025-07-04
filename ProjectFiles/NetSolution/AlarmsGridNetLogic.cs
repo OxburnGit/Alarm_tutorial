353db0e9e29409f4020b8bc0d20fe7ca19c00fa0
BrunoOptix <bruno.richardon@accenture.com>
2025-07-04 10:40:30 +0200

    First commit Alarm

---
+++ ProjectFiles/NetSolution/AlarmsGridNetLogic.cs
@@ -0,0 +1,29 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.UI;
+using FTOptix.SerialPort;
+using FTOptix.Retentivity;
+using FTOptix.NetLogic;
+using FTOptix.CoreBase;
+using FTOptix.NativeUI;
+using FTOptix.System;
+using FTOptix.Alarm;
+using FTOptix.UI;
+using FTOptix.HMIProject;
+using FTOptix.Core;
+#endregion
+
+public class AlarmsGridNetLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
